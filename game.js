ae8d9a73bc2b5fbadb928ea6987c0e867f577204
jurop26 <jurop26@azet.sk>
2023-08-04 07:14:34 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
+
+
+
+
+
+
+
+
+
+
 import { c, background, gameBar} from "./view.js";
 import {player1, Shoot, ItemsForCollection} from "./player.class.js";
 import {enemiesRelease, enemyShoots, enemies, Explosion} from "./enemies.class.js";
